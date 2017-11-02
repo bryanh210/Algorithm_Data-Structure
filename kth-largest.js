a5d25b5717c196e30fec896b9bd8b24e62532fa9
bryanh210 <tuongh@sas.upenn.edu>
2017-11-02 14:40:27 -0700

    kth

--- kth-largest.js
+++ kth-largest.js
@@ -1,1 +1,12 @@
 // http://www.geeksforgeeks.org/kth-largest-element-in-bst-when-modification-to-bst-is-not-allowed/
+
+// pseudo:
+// result = 0;
+//traversing in reverse in-order
+// push to an array
+// look up k by index
+// return result at the end
+
+function kthLargest(node){
+
+}
