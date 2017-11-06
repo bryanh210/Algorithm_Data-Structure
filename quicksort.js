21f21249b65886aa49ba5755e49de6e82b3334a6
bryanh210 <tuongh@sas.upenn.edu>
2017-11-05 22:05:13 -0800

    quicksort

---
+++ quicksort.js
@@ -0,0 +1,25 @@
+
+// in place
+function quickSort(input){
+  function divide(start,end){
+    let wall = start;
+    let pivot = end;
+
+    for(const i = start; i < end; i++){
+      // smaller number goes to left before the wall
+      if(arr[pivot] > arr[i]){
+        [arr[i], arr[wall]] = [arr[wall], arr[i]];
+        // increase wall to update wall position
+        wall ++;
+      }
+    }
+    // at the end switching pivot and wall
+    [arr[wall], arr[pivot]] = [arr[pivot], arr[wall]];
+    divide(0, wall-1);
+    // we don't want the wall, append it at the end
+    divide(wall+1, end)
+  }
+  
+  return input;
+
+}
