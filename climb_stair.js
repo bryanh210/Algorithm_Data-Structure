2122561320210bed8a71e381de1f7f41ae56993e
bryanh210 <tuongh@sas.upenn.edu>
2017-11-13 18:25:07 -0800

    climb Stair

---
+++ climb_stair.js
@@ -0,0 +1,36 @@
+Ex:Climb stairs
+You can move up 1,2 or 3 stairs at a time
+6 steps
+Can climb up 1, 2 or 3 stairs
+So how many unique way can we get to top of flight?
+
+      6
+    / |  \
+  5   4   3
+/ | \
+4 3 2  1 2 1
+
+base case: return number of ways
+if(n === 0) return 1 // if it's 0 its considered 1 way, just in that case
+if(n ===1), return 1
+if( n ===2)  return 2
+
+
+function climbStairs(input){
+
+    function countWays(n){
+        if (n === 0){
+            return 1;
+        } else if (n === 1){
+            return 1;
+        } else if (n === 2){
+            return 2;
+        }
+
+        return countWays(n-1) + countWays(n-2) + countWays(n-3);
+    }
+    return countWays(input);
+}
+
+
+console.log(climbStairs(6));
---
+++ lattice_path_memoize.js
@@ -0,0 +1,26 @@
+//Lattice path:
+go from bottom right to top left
+
+(0,0)
+
+
+          (n,n)
+
+
+Moemoizatio n reduces time complexity from O(2^n) to O(n)
+
+
+lattice_paths(x,y,cache:None)
+if cache is None, initialize it {0,0): 1}
+
+if x < 0 or y < 0,
+
+// if we go out of bound we have to bring it back to get to 0 (the end)
+  return 0
+
+  if(x,y)  in cache
+  return cache[(x,y)]
+  cache[x,y] = lattice_paths(x-1, y) + lattice_path(x, y-1)
+
+
+return cache([x,y]), cache
