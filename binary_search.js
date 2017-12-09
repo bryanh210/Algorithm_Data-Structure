a5521cf0c6e9614c1e3a4625b226efdd5b5d2ce4
bryanh210 <tuongh@sas.upenn.edu>
2017-12-09 01:22:20 -0800

    binarySearch

---
+++ binary_search.js
@@ -0,0 +1,40 @@
+/*pseudocode:
+have variable midpoint
+min = 0; max = arr.length -1
+
+condition is min >= max
++) if midpoint = target, return the index (guess)
+=) if midpoint < target, search the right of midpoint by increasing the min to be search +1,
+=) if midpoint < target, search the right of midpoint by increasing the min to be search +1,
+
+return -1 if not found
+
+
+*/
+
+function binarySearch(arr,target){
+  let guess, min = 0, max = arr.length-1;
+
+  if(arr.length === 0) { return -1};
+
+  while(min <= max){
+    guess = Math.floor((min+max)/2);
+    if(target === arr[guess]){
+      return guess;
+    } else if(target < arr[guess]){
+      max = guess -1;
+
+    } else if(target > arr[guess]) {
+      min = guess + 1;
+    } else{
+      return -1;
+    }
+  }
+
+
+}
+
+
+console.log(binarySearch([], 4))
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 4))
