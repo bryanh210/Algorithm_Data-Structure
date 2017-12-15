91ad3fec640e78a685620c88a076cb5a3590ce9f
bryanh210 <tuongh@sas.upenn.edu>
2017-12-15 14:38:49 -0800

    flattenArray

---
+++ cycle_undirectedGraph.js
@@ -0,0 +1,4 @@
+Cycle in an undirected graph:
+
+Traverse through it, if you hit the same node twice => cycle.
+Why? Because 1 time doesn't say enough since you basically can travel back and forth
---
+++ quickSort_review.js
@@ -0,0 +1,11 @@
+need 3 thing:
+pivot
+wall: To the left everything is smaller, to the right everything is bigger
+current
+
+Always swap current with wall
+Compare current to pivot.
+If current < pivot, current increment +1;
+if current > pivot, current
+
+sorted
