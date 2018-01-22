c399fbc9db85318ce41c0d2c06a5732b17f7d869
bryanh210 <tuongh@sas.upenn.edu>
2018-01-22 14:24:27 -0800

    fib_memoize

---
+++ fib_memoize.js
@@ -0,0 +1,19 @@
+function fib(num){
+  let memo = {}:
+
+  if(num < 0) { return new Error("Only positive number accepted")};
+  if(num === 0 || num === 1){
+    return num
+  }
+
+  if(memo.hasOwnProperty(num)){
+    return memo[num]
+  }
+
+  let result = fib(num -1) + fib(num -2);
+  memo[num] = result;
+
+  return result;
+
+
+}
