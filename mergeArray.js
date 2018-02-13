f34f3e7b81a68d99b4197b11071dbe477c4c0538
bryanh210 <tuongh@sas.upenn.edu>
2018-02-12 16:13:19 -0800

    mergeArray comments

--- mergeArray.js
+++ mergeArray.js
@@ -6,8 +6,10 @@ var mergeOrderArr = function (arr1,arr2){
     while( i < arr1.length  && j < arr2.length ){
 
       if(arr1[i] <= arr2[j]){
-
+//even if you push it in, the original array would not change
         result.push(arr1[i++]);
+        // equivalent to pushing i in then increase after
+        // if I want to push in i+1, I have to use ++1
 
 
       } else{
@@ -17,7 +19,7 @@ var mergeOrderArr = function (arr1,arr2){
       }
 
     }
-    console.log(result)
+    console.log(j)
   return result.concat(arr1.slice(i)).concat(arr2.slice(j))
 
 }
@@ -25,3 +27,35 @@ var mergeOrderArr = function (arr1,arr2){
 
 
 console.log(mergeOrderArr([1,100], [3,4,6,8]))
+
+
+
+var mergeOrderArr = function (arr1,arr2){
+    var result = [],
+     i = 0,
+     j = 0;
+
+    while( i < arr1.length  && j < arr2.length ){
+
+      if(arr1[i] <= arr2[j]){
+//even if you push it in, the original array would not change
+        result.push(arr1[i++]);
+        // equivalent to pushing i in then increase after
+        // if I want to push in i+1, I have to use ++1
+
+
+      } else{
+
+        result.push(arr2[j++]);
+
+      }
+
+    }
+  // must have this line because if i and j of arr1[i] and arr[2] hit length in the loop, the loop will break
+  return result.push(arr1.splice(i)).concat(arr2.splice(j))
+
+}
+
+
+
+console.log(mergeOrderArr([1], [0,2]))
